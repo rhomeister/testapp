0fd483f980591a8159d3d20362d8537f4a1fbbe2
rhomeister <r.stranders@gmail.com>
2015-04-25 00:00:50 +0200

    initial import

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+App::Application.routes.draw do
+  root "pages#home"
+  get "home", to: "pages#home", as: "home"
+  get "inside", to: "pages#inside", as: "inside"
+
+  resources :posts, only: [:show, :index]
+
+  devise_for :users
+
+  namespace :admin do
+    root "base#index"
+    resources :users
+    get "posts/drafts", to: "posts#drafts", as: "posts_drafts"
+    get "posts/dashboard", to: "posts#dashboard", as: "posts_dashboard"
+    resources :posts
+  end
+
+end
